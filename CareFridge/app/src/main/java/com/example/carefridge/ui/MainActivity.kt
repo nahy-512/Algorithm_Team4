d2b0248c19fb97d23aad3b3e5541a37040d9bf01
Nahyun Kim <hedghog1004@naver.com>
2023-11-28 00:22:38 +0900

    ✨ Feat : 바텀네비에 플로팅 버튼 추가

--- CareFridge/app/src/main/java/com/example/carefridge/ui/MainActivity.kt
+++ CareFridge/app/src/main/java/com/example/carefridge/ui/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.carefridge.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import com.example.carefridge.R
 import com.example.carefridge.databinding.ActivityMainBinding
@@ -14,7 +15,6 @@ class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::infl
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
 
         showInit()
         initBottomNav()
@@ -28,15 +28,21 @@ class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::infl
 
     private fun initBottomNav() {
         binding.mainBottomNav.itemIconTintList = null
+        // 바텀네비게이션 음영 삭제
+        binding.mainBottomNav.background = null
+        // 자리를 비워놓은(플로팅 버튼) 아이템에 대해 비활성화
+        binding.mainBottomNav.menu.getItem(1).isEnabled = false
 
         binding.mainBottomNav.setOnItemSelectedListener {
             when(it.itemId) {
                 R.id.menu_home -> {
                     HomeFragment().changeFragment()
+                    Log.d("MainActivity", "HomeFragment")
                 }
 
                 R.id.menu_list -> {
                     ListFragment().changeFragment()
+                    Log.d("MainActivity", "ListFragment")
                 }
             }
             return@setOnItemSelectedListener true
