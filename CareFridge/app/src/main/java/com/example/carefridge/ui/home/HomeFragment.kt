a8edbd62533d5c3d4ffbf14f5430517197969ab1
Nahyun Kim <hedghog1004@naver.com>
2023-12-03 11:58:57 +0900

    ✨ Feat : 홈 화면 레이아웃 구현

--- CareFridge/app/src/main/java/com/example/carefridge/ui/home/HomeFragment.kt
+++ CareFridge/app/src/main/java/com/example/carefridge/ui/home/HomeFragment.kt
@@ -2,6 +2,7 @@ package com.example.carefridge.ui.home
 
 import android.os.Bundle
 import android.view.View
+import android.view.animation.AnimationUtils
 import com.example.carefridge.R
 import com.example.carefridge.databinding.FragmentHomeBinding
 import com.quintable.jpower.config.BaseFragment
@@ -10,7 +11,18 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::bind
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+    }
 
+    override fun onStart() {
+        super.onStart()
 
+        setBlinkAnimation()
+    }
+
+    private fun setBlinkAnimation() {
+        // blink 애니메이션
+        val anim = AnimationUtils.loadAnimation(requireContext(), R.anim.blink_animation)
+        // 애니메이션 재생
+        binding.homeRecommendBtn.startAnimation(anim)
     }
 }
