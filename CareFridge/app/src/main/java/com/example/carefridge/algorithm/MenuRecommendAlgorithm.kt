fead44329e4506572d24c0d7393ad776c4f29b75
Nahyun Kim <hedghog1004@naver.com>
2023-12-04 18:08:23 +0900

    🐛 Fix : 메뉴 추천 알고리즘의 가중치 부여 코드 수정

--- CareFridge/app/src/main/java/com/example/carefridge/algorithm/MenuRecommendAlgorithm.kt
+++ CareFridge/app/src/main/java/com/example/carefridge/algorithm/MenuRecommendAlgorithm.kt
@@ -16,12 +16,8 @@ object MenuRecommendAlgorithm {
         // 인자로 잘 넘어왔는지 로그 확인
         Log.d("RecommendAlgo", "재료: ${ingredients},\n레시피: ${recipes},\n사용자 선호 음식: $userPreferences")
 
-        val recorecommendMenu = recommendMenu(ingredients, recipes)
-
-        // 메뉴 추천 및 결과 출력
-        Log.d("RecommendAlgo", "추천 음식: $recorecommendMenu")
         // 추천 받은 메뉴를 반환
-        return recorecommendMenu
+        return recommendMenu(ingredients, recipes)
     }
 
     // 사용자의 선호 재료 목록 초기화
@@ -41,29 +37,35 @@ object MenuRecommendAlgorithm {
             // 현재 레시피에 필요한 각 재료에 대해 점수를 계산하고 더함
             for (data in recipe.ingredients) {
                 val ingredientName = data.first
-                if (userPreferences!!.contains(ingredientName)) {
-                    // 사용자가 선호하는 재료에 해당하는 경우 가중치 부여
-                    for (ingredient in ingredients) {
-                        if (ingredient.name == ingredientName) {
-                            // 가중치 부여
-                            score += calculateWeight(ingredient)
-                        }
+                for (ingredient in ingredients) {
+                    // 양과 유통기한을 고려해서 가중치 계산 진행
+                    if (ingredient.name == ingredientName) {
+                        // 가중치 계산 결과를 score에 저장
+                        score += calculateWeight(ingredient)
+                    }
+                    /// 사용자가 선호하는 재료가 들어간 레시피에는 가중치를 추가 부여
+                    if (userPreferences!!.contains(ingredientName)) {
+                        // 추가 가중치 부여
+                        score += 100
                     }
                 }
             }
 
-            // 사용자가 선호하는 재료가 있다면, 사용된 레시피에만 가중치를 부여 / 또는 아예 없을 때
-            if (score > 0 || userPreferences!!.isEmpty()) {
-                // 사용자가 선호하는 재료가 사용된 경우에만 최종 점수를 계산
-                score /= recipe.ingredients.size // 재료 수로 정규화
+            // 최종적으로 현재의 레시피에서 가중치가 가장 높은 메뉴 저장
+            if (score > 0) {
+                score /= recipe.ingredients.size // score를 재료 수로 정규화
 
-                // 최고 점수보다 현재 레시피의 점수가 높으면 갱신
+                // 현재 저장된 최고 점수보다 높다면 추천 메뉴 업데이트
                 if (score > bestScore) {
                     bestScore = score
                     recommendedMenu = recipe.name
                 }
             }
         }
+        // 메뉴 추천 결과 출력
+        Log.d("RecommendAlgo", "추천 음식: $recommendedMenu")
+
+        // 추천 알고리즘을 통해 얻은 추천 메뉴를 반환
         return recommendedMenu
     }
 
